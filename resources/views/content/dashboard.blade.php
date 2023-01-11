9dab1667a64b175398657bf5649033121f5ad872
sandraaro <sandra16bb@gmail.com>
2023-01-11 12:45:50 -0400

    mejoras del dashboard y ruta

---
+++ resources/views/content/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+
+    <div class="row align-items-center bg-light m-0 vh-100" style="background-image: linear-gradient(to top, rgba(201, 184, 240, 0.5) 0%, rgba(255, 144, 79, 0.9) 100%),url( {{ asset('images/background.jpg') }});background-size: cover;background-attachment: fixed;">
+        <div class="col-6 d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('images/incidencias.svg')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Título de la tarjeta</h5>
+                  <p class="card-text">Un texto de ejemplo rápido para colocal cerca del título de la tarjeta y componer la mayor parte del contenido de la tarjeta.</p>
+                  <a href="#" class="btn btn-primary">Ir a algún lugar</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 d-flex justify-content-around">
+            <div class="card" style="width: 18rem;">
+                <img src="{{ asset('images/recordatorios.svg')}}" class="card-img-top" alt="...">
+                <div class="card-body">
+                  <h5 class="card-title">Título de la tarjeta</h5>
+                  <p class="card-text">Un texto de ejemplo rápido para colocal cerca del título de la tarjeta y componer la mayor parte del contenido de la tarjeta.</p>
+                  <a href="#" class="btn btn-primary">Ir a algún lugar</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('content.incidencias');
 });
+
+Route::get('/dashboard', function () {
+    return view('content.dashboard');
+});
+
